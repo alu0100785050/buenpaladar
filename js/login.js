dc978f10a52d8b30224d237ae98a110ce4ba7a8d
Fernando González <alu0101045870@ull.edu.es>
2018-12-06 16:56:20 +0000

    Rebase

--- js/login.js
+++ js/login.js
@@ -1,11 +1,33 @@
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if ( username === "Joey" && password === "VivaEspaña"){
-        window.location = "index.html"; // Redirecting to other page.
-        return true;
-    }
-    else{
-        location.reload(true);      //Refresh page
-    }
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
+
+function validate(){
+
+    var config = {
+      apiKey: "AIzaSyCrXSZWpBdn8HTiwEEySQ9Q4r4ZOE5jwIU",
+      authDomain: "practicasipc-e2448.firebaseapp.com",
+      databaseURL: "https://practicasipc-e2448.firebaseio.com",
+      projectId: "practicasipc-e2448",
+      storageBucket: "practicasipc-e2448.appspot.com",
+      messagingSenderId: "110438816831"
+    };
+    firebase.initializeApp(config);
+
+    const btnLogin = document.getElementById('submit');
+
+    btnLogin.addEventListener('click', e =>{
+
+        const email= document.getElementById('username').value;
+        const pass= document.getElementById('password').value;
+        const auth= firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(email,pass);
+        promise.catch(e =>alert('Error message: '+ e.message));
+
+    });
+
+}
+
+validate();
