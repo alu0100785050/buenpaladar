d47e867e477588c225b8f3341e2b8e1a14645b75
Fernando González <alu0101045870@ull.edu.es>
2018-12-30 21:01:58 +0000

    CAMBIO RADICAL EN ESTRUCTURA
    En local funciona, pero el servicio de hosting no está aún funcionando bien. No se cargan los recursos más allá de .html

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validate(){
+
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    if(validateEmail(email.value)){
+        const auth = firebase.auth();
+
+        auth.signInWithEmailAndPassword(email.value,password.value).then(function(){
+            console.log("Login successful!");
+
+            //Setup document cookie
+            document.cookie = "email=" + email;
+
+            window.location.href = "index.html"
+
+        }).catch(function(error){
+            //Handle errors
+
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log('Error: ' + errorCode + ', ' + errorMessage);
+
+            email.style.color = "coral";
+            password.style.color = "coral";
+        });
+
+    }
+    else{
+        email.style.color = "coral";
+        password.style.color = "coral";
+        password.value = '';
+    }
+
+    password.addEventListener('click', e => {
+        document.getElementById('password').value = '';
+    });
+}
