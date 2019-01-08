88d9fe3df3fb6ab8a1ebabe08422abbdb05b0cf1
Fernando González <alu0101045870@ull.edu.es>
2019-01-08 22:06:40 +0000

    Profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,26 +1,70 @@
+function changeToEdit(){
+     var edit_surn = document.getElementById("edit_surn"); //
+     var prof_surn = document.getElementById("prof_surn"); //
+     var edit_desc = document.getElementById("edit_description"); //
+     var prof_desc = document.getElementById("prof_description"); //
+
+     save_butt = document.getElementById("change_to_prof"); //
+     edit_butt = document.getElementById("change_to_edit"); //
+
+     edit_surn.style.display = "block";
+     edit_desc.style.display = "block";
+     save_butt.style.display = "block";
+
+     prof_surn.style.display = "none";
+     prof_desc.style.display = "none";
+     edit_butt.style.display = "none";
+
+}
+
+function changeToProf(){
+    var edit_surn = document.getElementById("edit_surn"); //
+    var prof_surn = document.getElementById("prof_surn"); //
+    var edit_desc = document.getElementById("edit_description"); //
+    var prof_desc = document.getElementById("prof_description"); //
+
+    save_butt = document.getElementById("change_to_prof"); //
+    edit_butt = document.getElementById("change_to_edit"); //
+
+    edit_surn.style.display = "none";
+    edit_desc.style.display = "none";
+    save_butt.style.display = "none";
+
+    prof_surn.style.display = "block";
+    prof_desc.style.display = "block";
+    edit_butt.style.display = "block";
+
+}
+
 function initApp(){
     //Listener para AuthStateChanged
     firebase.auth().onAuthStateChanged(function(user) {
 
        if (user) {
 
+            var edit_surn = document.getElementById("edit_surn"); //
             var prof_name = document.getElementById("prof_name");
-            var prof_surn = document.getElementById("prof_surn");
+            var prof_surn = document.getElementById("prof_surn"); //
             var prof_mail = document.getElementById("prof_email");
-            var prof_desc = document.getElementById("prof_description");
+            var edit_desc = document.getElementById("edit_description"); //
+            var prof_desc = document.getElementById("prof_description"); //
 
+            save_butt = document.getElementById("change_to_prof"); //
+            edit_butt = document.getElementById("change_to_edit"); //
             log_butt = document.getElementById("login_button");
+
             var displayName = user.displayName;
             var email = user.email;
             var uid = user.uid;
 
-            console.log(uid);
-            console.log(displayName);
-            console.log(email);
-
             SignedDisplay();
             prof_name.innerHTML = displayName;
             prof_mail.innerHTML = email;
+
+            //Default display
+            edit_surn.style.display = "none";
+            edit_desc.style.display = "none";
+            save_butt.style.display = "none";
 
             database.ref('/users/' + displayName).once('value').then(function(snapshot) {
                 prof_surn.innerHTML = (snapshot.val() && snapshot.val().surName) || '--';
